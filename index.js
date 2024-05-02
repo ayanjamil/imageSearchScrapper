8bfcef6eda97935d28df27290e844a0e59c8b333
ayanjamil <ayanjamil00@gmail.com>
2024-05-03 00:10:16 +0530

    init

---
+++ index.js
@@ -0,0 +1,53 @@
+const pup = require('puppeteer');
+
+let browser, page;
+
+async function openGoogleLens() {
+    console.log("Opening Google Lens");
+    browser = await pup.launch({
+        headless: false,
+        defaultViewport: false,
+        userDataDir: "./tmp",
+    });
+    page = await browser.newPage();
+    await page.goto('https://google.com/');
+}
+
+async function searchUrl(url) {
+    console.log("searching url");
+    await page.click('div[jscontroller="lpsUAf"][jsname="R5mgy"]'); // google lense button 
+    await page.type('input[jsname="W7hAGe"][class="cB9M7"]', url); // Paste image link field
+    await page.click('div[jsname="ZtOxCb"][class="Qwbd3"]'); // search button
+
+}
+async function getResults() {
+    await new Promise(resolve => setTimeout(resolve, 15000));
+    const cards = await page.evaluate(() => {
+        const cardList = document.querySelectorAll(".G19kAf ");
+        return Array.from(cardList).map((card) => {
+            const title = card.querySelector(".UAiK1e").innerText;
+            const storeName = card.querySelector(".OwskJc").innerText;
+            const productImageUrl = card.querySelector("img.wETe9b.jFVN1").getAttribute('src');
+            const productLink = card.querySelector("a.lXbkTc").getAttribute('href');
+            // const price = card.querySelector(".oOZ3vf.DdKZJb").innerText;
+            return { title, storeName, productImageUrl, productLink };
+        });
+    });
+    console.log(cards);
+}
+
+
+
+async function closeBrowser() {
+    console.log("closing Browser");
+    await new Promise(resolve => setTimeout(resolve, 3000));
+    await browser.close();
+}
+
+async function run() {
+    await openGoogleLens();
+    await searchUrl('https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcQ2I-32bc1Ua0bz-TbGOMTE4L_zHX-3oLsdJHEREJQ6tHuzuD6V');
+    await getResults();
+}
+
+run();
---
+++ usables.js
@@ -0,0 +1,150 @@
+export default [
+  {
+    in_stock: true,
+    link: 'https://poshmark.com/listing/Mister-Zimi-Sloane-Jeans-in-Tan-6286ca9a5e0ba8bcd3f6c929',
+    position: 21,
+    price: { currency: '$', extracted_value: 90, value: '$90.00*' },
+    source: 'Poshmark',
+    source_icon:
+      'https://encrypted-tbn0.gstatic.com/favicon-tbn?q=tbn:ANd9GcRglTQayecXuxgCFUZcgfbfA0OslKHNi_zul24ja6dNR7fFy_QNTwAYGBTDRYwPhEq6SEu7vqbjxSSTFT8JfQQODue5hNhO-4uQ26dgqP4HA44',
+    thumbnail:
+      'https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcQ2I-32bc1Ua0bz-TbGOMTE4L_zHX-3oLsdJHEREJQ6tHuzuD6V',
+    title: 'Mister Zimi Sloane Jeans in Tan',
+  },
+  {
+    in_stock: true,
+    link: 'https://www.alibaba.com/product-detail/Dropshipping-Women-High-Waist-Stretchable-Spring_62458216916.html',
+    position: 22,
+    price: { currency: '$', extracted_value: 8.99, value: '$8.99*' },
+    source: 'Alibaba',
+    source_icon:
+      'https://encrypted-tbn3.gstatic.com/favicon-tbn?q=tbn:ANd9GcS_WLPDgDq-t35qQTdfqER8IlxaxHIeTh0X1Tm3cHpEbo9dnaycBZp9l7mlat2aRoJu4RgAEKTOmkMNRvN2wtRhAE44gib3sDVkz8CJ3EebcpCRkCs',
+    thumbnail:
+      'https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcRMt3bQG1nCFXFc_AKlprcnglZ3kPUOd7vHg7pAon5Ug9vuUzs0',
+    title:
+      'Dropshipping Women High Waist Stretchable Spring Autumn Female Casual Belt Patchwork Long Trousers Work Wear Lady Office Pant',
+  },
+  {
+    in_stock: true,
+    link: 'https://www.jumia.com.eg/fashion-5485-khakihigh-waist-office-lady-pants-korean-fashion-ladies-full-length-straight-pants-women-formal-work-wear-solid-trousers-wef-40980640.html',
+    position: 23,
+    price: { currency: 'E', extracted_value: 1290, value: 'EGP 1,290.00*' },
+    source: 'Jumia Egypt',
+    source_icon:
+      'https://encrypted-tbn1.gstatic.com/favicon-tbn?q=tbn:ANd9GcRo5YL5MlKhYxhYLVzjzg1HFaR43ILiRhkxnlwmNEeA-Zgl_SaIRZwUpwMtpaSErCJplc1RCozcyBxBT9kjIt6q_72RhTjKT5P9lZ2V4TMywROoyAJa',
+    thumbnail:
+      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSGXoQMAsAdnMdYQ2C57Qz-vlSzMPp59AUdN3AMVgYiWXxPWtw9',
+    title:
+      '(5485 Khaki)High Waist Office Lady Pants Korean Fashion Ladies Full-length Straight Pants Women Formal Work Wear Solid Trousers WEF',
+  },
+  {
+    in_stock: true,
+    link: 'https://idposter.com/Hayley-Atwell-posters-and-prints/711595-Hayley-Atwell-beach-tote-bag',
+    position: 28,
+    price: { currency: '$', extracted_value: 17.99, value: '$17.99*' },
+    rating: 4.1,
+    reviews: 7,
+    source: 'idPoster.com',
+    source_icon:
+      'https://encrypted-tbn3.gstatic.com/favicon-tbn?q=tbn:ANd9GcR9duGY2E5PqtuIevvA4WFcYWxulYzfCrty_ovUl2fXJEQUzlqCV9Y-Cnk8U-zhRVUehvVbHkoNa_ST-wEtRK1cK_BdNeM0EovdZSIIoc9EzUE',
+    thumbnail:
+      'https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcSXc6g3WeQKdu05fxotkOOi5njcQ7uDP-QUPQBbmrr8VKyEe2lW',
+    title: 'Hayley Atwell Tote Bag Print #711595 Online | idPoster.com',
+  },
+  {
+    in_stock: false,
+    link: 'https://www.carousell.ph/p/mango-straight-suit-trousers-brown-1287025822/',
+    position: 32,
+    price: { currency: '₱', extracted_value: 550, value: '₱550.00*' },
+    source: 'carousell.ph',
+    source_icon:
+      'https://encrypted-tbn1.gstatic.com/favicon-tbn?q=tbn:ANd9GcSeKpmrfODnsTKpFXV5vp8nzccEaKeP-pIWyiL-4UGEPhz3jyvsyhInU-qVUp6z4wI5gY4wGEXc5uBmAPZuL-AT3PW6tMEy4PFI6StMn4uQoJn--5lY',
+    thumbnail:
+      'https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcSAXZC2nMnjeTKA6KR4UNM8ggMOqV6N56-i-Ut2HO7TPmLDTyor',
+    title: 'Mango Straight Suit Trousers (Brown)',
+  },
+  {
+    link: 'https://www.saksfifthavenue.com/product/vince-high-waisted-cigarette-trousers-0400017022942.html',
+    position: 37,
+    price: { currency: '$', extracted_value: 345, value: '$345.00*' },
+    source: 'Saks Fifth Avenue',
+    source_icon:
+      'https://encrypted-tbn2.gstatic.com/favicon-tbn?q=tbn:ANd9GcSc957TvtEHfmU-SHgWcgqQuFnBkqcPl-gQcYp37Pc4acqoSvES2PgnBpPlg2itF8XeyVJZjdOLVs3RtHyE6xHCzkNIWWPKdrq4o26dDAF2F36iuimjB39jlWbsxw',
+    thumbnail:
+      'https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcTp5ZDHEXdMLGlnWsDs9a7aV3AdfNaxiN3eCmRiZ339mbeRzD4b',
+    title: 'Shop Vince High-Waisted Cigarette Trousers | Saks Fifth Avenue',
+  },
+  {
+    in_stock: true,
+    link: 'https://www.lorecarmet.com/us/ichi-alice-brown-beret-womens-hats-and-scarves/',
+    position: 38,
+    price: { currency: '$', extracted_value: 26, value: '$26.00*' },
+    source: 'lorecarmet.com',
+    source_icon:
+      'https://encrypted-tbn1.gstatic.com/favicon-tbn?q=tbn:ANd9GcRDKL3z7Q5IzCT75W-hRrfsmC5aA77sO6KEAGUL8w7dBJXmwww9JKw25fhowLhQ3yFSF1aTbZrPOdTICjSyAmWbrBIrBIaprH1X24P7JOBe72ytwhlhyb8',
+    thumbnail:
+      'https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcTXQBe1x_YRHuqOqC7gqMWwDMDRv4vBVgpa8F5pGStDZ-XDAnXr',
+    title: 'Ichi Alice Brown Beret | Womens Hats And Scarves',
+  },
+  {
+    in_stock: true,
+    link: 'https://lulu.ie/product/zella-turn-up-trousers/',
+    position: 40,
+    price: { currency: '€', extracted_value: 99.95, value: '€99.95*' },
+    source: 'lulu.ie',
+    source_icon:
+      'https://encrypted-tbn1.gstatic.com/favicon-tbn?q=tbn:ANd9GcSq5uWBkU8hzXAAjzgkVUlp8Au44Y5DACwuhwPFWp0tX5PA3nRvT80-VwBwiaxygtlcirPBmlo-gHGMPLje1barNjQFJUMTsDx8jzV4',
+    thumbnail:
+      'https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcS04eEnzgOljb5NAIFEdKIyN6MGBp48VEpRT8iGdm1G2QL68phi',
+    title: 'Zella turn up trousers',
+  },
+  {
+    in_stock: true,
+    link: 'https://www.breuninger.com/nl/en/b/raffaello-rossi/trousers-dora/1001332319/p/',
+    position: 53,
+    price: { currency: '€', extracted_value: 129.99, value: '€129.99*' },
+    source: 'BREUNINGER',
+    source_icon:
+      'https://encrypted-tbn2.gstatic.com/favicon-tbn?q=tbn:ANd9GcRO8vYgAv0z3On9iZ_Z6juLnuMbaFv4oAcuC-2AP2G2hs90fK4-WGyf_AjFRqT1iDPYuXuepzls72jHmSVBuzvrPNwbE-hb4So7e9iqdO5NXyu271luqeM',
+    thumbnail:
+      'https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcTmaveAD0ukyp9IXYnnw1LL67ox-CTOZLrc3VPVKHikzIzToytQ',
+    title: 'RAFFAELLO ROSSI Trousers DORA',
+  },
+  {
+    in_stock: false,
+    link: 'https://bananarepublic.gap.com/browse/product.do?pid=615302032',
+    position: 55,
+    price: { currency: '$', extracted_value: 52.97, value: '$52.97*' },
+    rating: 3,
+    reviews: 5,
+    source: 'Gap',
+    source_icon:
+      'https://encrypted-tbn2.gstatic.com/favicon-tbn?q=tbn:ANd9GcRd17QZn1lSq8anO9FF3FOGOxL_pAKm9nL1-p2TsswHloCGWlgYOOJtxaFeBYkAVMoWOKa_sIcjfQ0zBBo0T_BbBex3OO2Eh6GtRhBTkP_FlLCNQT_epCKO1kk8',
+    thumbnail:
+      'https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcTGQLYNWPtQa6UBi9tHBjCamMFtNjVOywBEvwPXNbEHsTscKyZV',
+    title: 'Ryan Slim-Fit Washable Wool-Blend Pant',
+  },
+  {
+    in_stock: true,
+    link: 'https://www.debenhams.com.kw/en/buy-slim-ankle-grazer/-color-camel.html',
+    position: 59,
+    price: { currency: 'K', extracted_value: 9, value: 'KWD 9.000*' },
+    source: 'Debenhams Kuwait',
+    source_icon:
+      'https://encrypted-tbn2.gstatic.com/favicon-tbn?q=tbn:ANd9GcRWlNjBIH-taqYCFt84e4dnCDN3b5-30XV2pb6-QSz9gYB-m5MwuQ9BMkW2qz4nnBuh2yZWClWrnvh0htzt-cs7vcG8H1StLHJTvIxDhYwaqhIf3kLrUbnMgw',
+    thumbnail:
+      'https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcRbGavehnfhO4sGPtpvdQhZjZeaEFH0F1kDDICLtYmkLx5bPA22',
+    title: 'Slim Ankle Grazer',
+  },
+  {
+    link: 'https://www.dorothyperkins.com/product/dorothy-perkins-tall-camel-ankle-grazer_aqq42517',
+    position: 60,
+    price: { currency: '£', extracted_value: 14, value: '£14.00*' },
+    source: 'Dorothy Perkins',
+    source_icon:
+      'https://encrypted-tbn3.gstatic.com/favicon-tbn?q=tbn:ANd9GcRNsFAAync4SWBkQ443o412JmBJsMkqAUm_EsgJl8C7VcDfnM7O2S0FVu5r0yjlBQPdTmCwTo5eGnbBkveiQWByqeLi3PY0iChfxlNf_FPS-E-9JO3_Sw5_6XW9',
+    thumbnail:
+      'https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcQxrbqxC95lxHUatrvjJWpTomTh6YUVic5Mjsn49gmncTbo-iF-',
+    title: 'Tall Camel Ankle Grazer',
+  },
+];
